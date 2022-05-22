657c3263094ca090430ba7a483722f6b813c5f78
HariGoutham <goutham540@gmail.com>
2022-05-23 03:40:08 +0530

    refactored

--- src/main/java/com/example/demo/y1_handy/Test.java
+++ src/main/java/com/example/demo/y1_handy/Test.java
@@ -1,10 +1,10 @@
 package com.example.demo.y1_handy;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Test {
 
-    public static void main(String[] args) {
-
-
-    }
 
 }
---
+++ src/main/java/com/example/demo/y1_handy/TestArchive.java
@@ -0,0 +1,21 @@
+package com.example.demo.y1_handy;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class TestArchive {
+
+    public static void main(String[] args) {
+        collectDistinctEvenNumbers();
+    }
+    private static void collectDistinctEvenNumbers() {
+        Integer[] integers = {1, 2, 3, 2, 5, 4, 9, 7, 6, 6, 8, 3, 7, 4, 5, 66, 43, 34, 334, 3244, 8300};
+        System.out.println(Arrays.asList(integers).stream().distinct()
+                .filter(evenNumbers()).sorted().collect(Collectors.toList()));
+    }
+
+    private static Predicate<Integer> evenNumbers() {
+        return i -> i % 2 == 0;
+    }
+}
